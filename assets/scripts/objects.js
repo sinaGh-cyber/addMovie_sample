6776e767d0b126fad3bf22ff661cf33fa0dd1810
sinaGh-cyber <sinaina1377@gmail.com>
2021-06-09 02:11:47 +0430

    renderMovies function added

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,22 @@ const searchBtn = document.getElementById(`search-btn`);
 
 const movies = [];
 
+const renderMovies = () => {
+    const movieList = document.getElementById(`movie-list`);
+    if (movies.length === 0){
+        movieList.classList.remove(`visible`)
+    } else {
+        movieList.classList.add(`visible`)
+    };
 
+    movieList.innerHTML = ``;
+
+    movies.forEach( movie => {
+        const movieEl = document.createElement(`li`);
+        movieEl.textContent = movie.info.title;
+        movieList.append(movieEl);
+    });
+};
 
 const addMovieHandler = () => {
   const title = document.getElementById(`title`).value;
@@ -26,7 +41,7 @@ const addMovieHandler = () => {
     id: Math.random() * Math.random() * Math.random(),
   };
   movies.push(newMovie);
-  console.log(movies);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener(`click`, addMovieHandler);
